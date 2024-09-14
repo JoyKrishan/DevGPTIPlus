1e96d18e8815c7eb5c8416bd10973105e60b5f42
Joy Krishan Das <joykrishan10@gmail.com>
2024-09-14 16:08:52 -0600

    added more func to cal sentiments related to diff intents

--- src/rq4_sentiment/statistics.py
+++ src/rq4_sentiment/statistics.py
@@ -1,60 +1,56 @@
 import pandas as pd
 import os
 import json
-from scipy.stats import kruskal, mannwhitneyu
-import numpy as np
+from scipy.stats import kruskal, mannwhitneyu, spearmanr
 
-from bokeh.models import ColumnDataSource
-from bokeh.palettes import GnBu3, OrRd3
-from bokeh.plotting import figure, show
-
+def calculate_median_magnitude(df, col):
+    return df.groupby('Label')[col].median()
 
 def number_of_data_lean_pos_neg(df, col):
-    df.groupby('Label').size()
-    
-    for group, data in df.groupby('Label'):
-        positive_lean_count = df.groupby('Label')[col].apply(lambda x: (x > 0).sum())
-        negative_lean_count = df.groupby('Label')[col].apply(lambda x: (x < 0).sum())
-        print(f"Group: {group}")
-        print(f"Positive Lean Count: {positive_lean_count}")
-        print(f"Negative Lean Count: {negative_lean_count}")
+    positive_lean_count = df.groupby('Label')[col].apply(lambda x: (x > 0).sum())
+    negative_lean_count = df.groupby('Label')[col].apply(lambda x: (x < 0).sum())
+        
+    print(f"Positive Lean Count: {positive_lean_count}")
+    print(f"Negative Lean Count: {negative_lean_count}")
     print(f"Total Count: {len(df)}")
-    return group, positive_lean_count, negative_lean_count
-
-# def plot_pos_neg_lean(df):
-
-#     label_dev =['Dev_Ideation', 'Dev_Other', 'Dev_Refractoring', 'Dev_Synthesis', 'Dev_Troubleshooting', 'Dev_Understanding', 'Dev_Validation']
-#     label_gpt = ['GPT_Ideation', 'GPT_Other', 'GPT_Refractoring', 'GPT_Synthesis', 'GPT_Troubleshooting', 'GPT_Understanding', 'GPT_Validation']
     
+    return positive_lean_count, negative_lean_count, len(df)
 
 def clean_data(df):
     df = df.drop_duplicates(subset=['IssueID'])
-
     df = df.reset_index()
-
-    # Drop column: 'index'
     df = df.drop(columns=['index'])     
     return df  
 
+def calculate_corr(df, col1, col2):
+    
+    unique_labels = df['Label'].unique()
+    
+    for i, label in enumerate(unique_labels):
+    
+        group = df[df['Label'] == label]
+        dev_ema = group[col1]
+        gpt_ema = group[col2]
+
+        corr, _ = spearmanr(dev_ema, gpt_ema)
+        print(f"Pearson Correlation Coefficient for {label}: {corr}")
+        
 
 if __name__ == "__main__":
-    
     project_dir = os.path.join(os.path.abspath(os.path.join(__file__, '../../../')))
     sentiment_file = os.path.join(project_dir, 'sentiment_analysis_results_intents.csv')
     df = pd.read_csv(sentiment_file)
     df = clean_data(df)
     
-    number_of_data_lean_pos_neg(df, 'Dev Sentiment EMA')
-    # plot_pos_neg_lean(df)
-    # quali_file_path = os.path.join(data_dir, 'rq1', 'RQ1-Analysis.xlsx')
-    # complete_data_file_path = os.path.join(data_dir, 'complete_dataset', 'DevGPT_issues.json')
-    # kruskal_results = calculate_kruskal(df)
+    # print("Dev Sentiment EMA")
+    # number_of_data_lean_pos_neg(df, 'Dev Sentiment EMA')
+    # print("GPT Sentiment EMA")
+    # number_of_data_lean_pos_neg(df, 'GPT Sentiment EMA')
     
-    # # Print Kruskal results
-    # print("Kruskal Test Results:")
-    # print("Statistic:", kruskal_results.statistic)
-    # print("p-value:", kruskal_results.pvalue)
+    # print("Dev Sentiment EMA Median Magnitude")
+    # print(calculate_median_magnitude(df, 'Dev Sentiment EMA'))
+    # print("GPT Sentiment EMA Median Magnitude")
+    # print(calculate_median_magnitude(df, 'GPT Sentiment EMA'))
+    print("Correlation between Dev Sentiment EMA and GPT Sentiment EMA")
+    print(calculate_corr(df, 'Dev Sentiment EMA', 'GPT Sentiment EMA'))
     
-    # corr_mat = posthoc_mann_whitney_test(df)
-    # print("Correlation Matrix:")
-    # print(corr_mat)
